98ea4ed715bf6302bff6728befc78a2e2706cde1
javadAziz <azizbaylij@gmail.com>
2023-10-09 06:08:49 +0400

    Day of the Programmer challange solved

---
+++ Algorithms/DayOfTheProgrammer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'dayOfProgrammer' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER year as parameter.
+     */
+
+    public static string dayOfProgrammer(int year)
+    {
+        int date = 0;
+        string result = "";
+
+        if (year < 1918 & year % 4 == 0)
+        {
+            date = 256 - 244;
+        }
+        else if (year > 1918 & (year % 400 == 0 || (year % 4 == 0 & year % 100 != 0)))
+        {
+            date = 256 - 244;
+        }
+        else if (year == 1918)
+        {
+            date = 256 - 230;
+        }
+        else
+        {
+            date = 256 - 243;
+        }
+
+        result = date + ".09." + year;
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int year = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string result = Result.dayOfProgrammer(year);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
